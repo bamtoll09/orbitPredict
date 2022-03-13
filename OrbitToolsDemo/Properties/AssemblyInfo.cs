4bc159d74593df5c4212800fa3c9f9925ca2a756
박재현 <welon@bagjaehyeons-MacBook-Air.local>
2022-03-14 07:44:49 +0900

    first commit

---
+++ OrbitToolsDemo/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+//
+// Copyright (c) 2003-2021 Michael F. Henry
+// Version 03/2021
+//
+
+using System.Reflection;
+/*
+[assembly: AssemblyTitle("OrbitToolsDemo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Zeptomoby.com")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("(c) 2003-2021 Michael F. Henry")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyDelaySign(false)]
+[assembly: AssemblyVersion    ("1.4.0.0")]
+[assembly: AssemblyFileVersion("1.4.0.0")]
+*/
